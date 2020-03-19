90b4d260525886af98b1aee74732b40c4264ed91
vad9915 <49940065+vad9915@users.noreply.github.com>
2020-03-19 14:13:30 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var TelegramBot = require('node-telegram-bot-api');
+
+var token = 1007860085:AAEHDu3IZkZoptFcm5qePDA5s1yf4ZCZIwQ;
+var bot = new TelegramBot(token, {polling: true});
+
+var notes = [];
+
+bot.onText(/напомни (.+) в (.+)/, function (msg, match) {
+    var userId = msg.from.id;
+    var text = match[1];
+    var time = match[2];
+
+    notes.push({ 'uid': userId, 'time': time, 'text': text });
+
+    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
+});
+
+setInterval(function(){
+    for (var i = 0; i < notes.length; i++) {
+    const curDate = new Date().getHours() + ':' + new Date().getMinutes();
+    if (notes[i]['time'] === curDate) {
+      bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: '+ notes[i]['text'] + ' сейчас.');
+      notes.splice(i, 1);
+    }
+  }
+}, 1000);
